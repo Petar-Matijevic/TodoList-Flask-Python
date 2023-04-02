849bceaddccdf1a3098399260d65346490a6ae7b
Petar Matijević <66-2019@ftn.edu.rs>
2023-04-02 16:25:07 +0200

    ToDO Python and Flask Project

--- todo/app.py
+++ todo/app.py
@@ -7,6 +7,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 class Todo(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
